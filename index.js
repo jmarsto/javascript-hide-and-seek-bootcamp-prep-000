9fd6049d2b2046eadf6f8bbf92c13261b82f25a0
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-21 22:08:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector(".target")
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + parseInt(`${n}`)).toString()
+  }
+}
+
+function deepestChild() {
+  const divs = document.getElementById('grand-node').querySelectorAll('div')
+  return divs[3]
+}
